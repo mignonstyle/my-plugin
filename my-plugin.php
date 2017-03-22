967e7a75f7aac1124ea9417131ad17c5a0f67688
mignonstyle <mignonxstyle@gmail.com>
2017-03-23 04:55:24 +0900

    first commit

---
+++ my-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: My Plugin
+Plugin URI: https://github.com/mignonstyle/my-plugin
+Description: 練習用のプラグインです
+Version: 1.0
+Author: Mignon Style
+Author URI: http://mignonstyle.com/
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/

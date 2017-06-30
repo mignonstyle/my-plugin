13ce2a78738f17b7fcf195e8b50424b2fe28c3d9
mignonstyle <mignonxstyle@gmail.com>
2017-07-01 07:52:36 +0900

    modify travis test file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,9 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package my-plugin
+ * @package My_Plugin
  */
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
